00335dc019d4189b68dde126d0cbfadd7294530b
cafe51 <cafecafe51@hotmail.com>
2023-06-30 05:14:34 -0400

    chore: criado testes

---
+++ src/routes/index.ts
@@ -0,0 +1,4 @@
+import castCharacterRouter from './CastCharacterRoute';
+import castRouter from './CastRoute';
+import characterRouter from './CharacterRoute';
+export { characterRouter, castCharacterRouter, castRouter };
